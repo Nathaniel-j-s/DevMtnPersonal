3c9a2c7625ccc16be90d88688f7cb441113faed3
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2017-04-13 12:04:05 -0600

    still not super ready but lots more stuff

--- src/app/components/room/room.component.ts
+++ src/app/components/room/room.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FirebaseService} from '../../services/firebase.service';
+import {Router, ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-room',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room.component.css']
 })
 export class RoomComponent implements OnInit {
+  id: any;
+  room: any;
+  imageUrl: any;
 
-  constructor() { }
+  constructor(
+    private firebaseService:FirebaseService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+
+    this.firebaseService.getRoomDetails(this.id).subscribe(room => {
+      console.log(room);
+      this.room = room;
+    });
   }
 
 }
--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import {AngularFire, FirebaseListObservable} from 'angularfire2';
+import {AngularFire, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2';
 
 @Injectable()
 export class FirebaseService {
   rooms: FirebaseListObservable<any[]>;
+  room: FirebaseObjectObservable<any[]>;
   constructor(private af: AngularFire) { }
 
   getRooms() {
     this.rooms = this.af.database.list('/rooms') as FirebaseListObservable<Room[]>
     return this.rooms;
+  }
+
+  getRoomDetails(id) {
+    this.room = this.af.database.object('/rooms/'+id) as FirebaseObjectObservable<Room>;
+    return this.room;
   }
 
 }
