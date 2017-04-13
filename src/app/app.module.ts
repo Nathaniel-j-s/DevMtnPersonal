763d2a95eae9a443448ddfc9f1f7876aac69ac65
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2017-04-12 23:21:20 -0600

    rough skeleton and some firebase work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { EditRoomComponent } from './components/edit-room/edit-room.component';
+import { AddRoomComponent } from './components/add-room/add-room.component';
+import { RoomComponent } from './components/room/room.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAsJEPCKe9ls5f30s1aqf7u_RRTZR7Y8A8',
+  authDomain: 'devmtnpersonal.firebaseapp.com',
+  databaseURL: 'https://devmtnpersonal.firebaseio.com',
+  storageBucket: 'devmtnpersonal.appspot.com',
+  messagingSenderId: '579372619427'
+};
+
+const appRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'rooms', component:RoomsComponent},
+  {path:'calendar', component:CalendarComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RoomsComponent,
+    EditRoomComponent,
+    AddRoomComponent,
+    RoomComponent,
+    NavbarComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
